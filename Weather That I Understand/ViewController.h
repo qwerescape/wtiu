39d002b3af65e70a6eea035254672641848d976a
Chao Qu <chaoqu@Chaos-MacBook-Pro.local>
2013-04-19 21:26:15 -0400

    Initial Commit

---
+++ Weather That I Understand/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Weather That I Understand
+//
+//  Created by Chao Qu on 2013-04-19.
+//  Copyright (c) 2013 Chao Qu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Weather That I Understand/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Weather That I Understand
+//
+//  Created by Chao Qu on 2013-04-19.
+//  Copyright (c) 2013 Chao Qu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
